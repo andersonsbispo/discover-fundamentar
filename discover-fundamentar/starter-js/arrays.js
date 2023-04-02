a3b2d21c7a60598417040d88e57fb3bcb26cc0bd
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-04-02 15:36:34 -0300

    first commit

---
+++ discover-fundamentar/starter-js/arrays.js
@@ -0,0 +1,16 @@
+// Array
+
+const animals = [
+    "Lion",
+    "Monkey",
+    {
+        name: "Cat",
+        age: 3
+    }
+];
+
+// acessar valores dentro do Array
+console.log(animals[0]);
+
+// acessar objetos dentro do Array
+console.log(animals[2].name);
---
+++ discover-fundamentar/starter-js/scripts.js
@@ -0,0 +1,8 @@
+// Aqui eu tenho um comentário em linha
+/* 
+    Aqui eu tenho um comentário em bloco
+
+    O código abaixo escreve alguma mensagem no devtools
+*/
+console.log("Hello World"); // Alguma coisa
+//console.log("iria ignorar");
