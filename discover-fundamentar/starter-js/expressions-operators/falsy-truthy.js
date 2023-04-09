439c848ef930d75a4e8854c1285e8f8088d7446d
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-04-08 23:26:56 -0300

    Guia Estelar JavaScripit - Seção 8

--- discover-fundamentar/starter-js/expressions-operators/falsy-truthy.js
+++ discover-fundamentar/starter-js/expressions-operators/falsy-truthy.js
@@ -11,4 +11,22 @@
     NaN
 */
 
-console.log(true ? "verdadeiro" : "falso");
+console.log(0 ? "verdadeiro" : "falso");
+
+/*
+    TRUTHY
+    Quando um valor é considerado true em contextos onde um booleano é obrigatório (condicionais e loops)
+
+    true
+    {}
+    []
+    1
+    3.23
+    "0"
+    "false"
+    -1
+    Infinity
+    -Infinity
+*/
+
+console.log([] ? "verdadeiro" : "falso");
