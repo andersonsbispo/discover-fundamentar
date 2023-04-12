769be3be9b994b5872d34acad9c4dd7342894ea0
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-04-11 22:55:56 -0300

    Guia Estealr JavaScript - Seção 11 video 2

---
+++ discover-fundamentar/starter-js/sistema-notas-escolares.js
@@ -0,0 +1,11 @@
+/*
+    ### Transformar notas escolares
+
+    Crie um algoritmo que transforme as todas do sistema nuérico para de notas em caracteres tipo A B C
+
+    * de 90 para cima - A
+    * entre 80 - 89   - B
+    * entre 70 - 79   - C
+    * entre 60 - 69   - D
+    * menor que 60    - F
+*/
