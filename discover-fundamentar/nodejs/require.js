e6004e498b5854603371106a02c11c2680c7cf2c
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-06-09 15:00:40 -0300

    NodeJS - Seção 3

---
+++ discover-fundamentar/nodejs/require.js
@@ -0,0 +1,9 @@
+// Módulos nativos
+// const path = require("path");
+
+// console.log(path.basename("Users/starter/docs.js"));
+
+// Meus Módulos
+const myModule = require("./exports.js");
+
+console.log(myModule);
