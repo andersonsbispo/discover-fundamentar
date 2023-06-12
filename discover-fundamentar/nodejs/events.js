c3985f6e162d0c557bf1094b9b3a44a4131ff1d3
Anderson Bispo <andersonsbispo1997@outlook.com>
2023-06-11 23:12:31 -0300

    NodeJS

---
+++ discover-fundamentar/nodejs/events.js
@@ -0,0 +1,17 @@
+const {EventEmitter} = require("events");
+
+const ev = new EventEmitter();
+
+/*
+ev.once("saySomething", (message) => {
+    console.log("Eu ouvi você", message);
+});
+*/
+
+ev.on("saySomething", (message) => {
+    console.log("Eu ouvi você", message);
+});
+
+ev.emit("saySomething", "Anderson");
+ev.emit("saySomething", "João");
+ev.emit("saySomething", "Maria");
